7945b448d2ccd1e1fc9644b75573c4de1ca959e6
tian <tqj.zyy@gmail.com>
2016-10-12 14:10:49 +0800

    add MOE support

---
+++ html/src/net/mwplay/nativefont/client/HtmlLauncher.java
@@ -0,0 +1,28 @@
+package net.mwplay.nativefont.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.badlogic.gdx.graphics.Pixmap;
+
+import net.mwplay.nativefont.NativeFontListener;
+import net.mwplay.nativefont.NativeFontPaint;
+import net.mwplay.nativefont.test.FontTestGame;
+
+public class HtmlLauncher extends GwtApplication implements NativeFontListener {
+
+    @Override
+    public GwtApplicationConfiguration getConfig() {
+        return new GwtApplicationConfiguration(480, 800);
+    }
+
+    @Override
+    public ApplicationListener createApplicationListener() {
+        return new FontTestGame(HtmlLauncher.this);
+    }
+
+    @Override
+    public Pixmap getFontPixmap(String str, NativeFontPaint freePaint) {
+        return null;
+    }
+}
